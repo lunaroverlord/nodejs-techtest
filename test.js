3b4a58431f40ca50b5cba33af243c10b3780cd6f
Olafs Vandans <lunaroverlord@yahoo.com>
2016-05-04 11:54:18 +0300

    Tests for most of the functionality

---
+++ test.js
@@ -0,0 +1,82 @@
+var request = require('supertest');
+var app = require("./app.js");
+
+/*
+ * Tests assume that the provided database with 6 carers is connected on app
+ */
+
+describe('Skills getter works', function()
+{
+        /*
+         * 's' should return 'Autism'
+         */
+        it('Returns the expected skill from a simple query', function(done)
+        {
+                request(app)
+                        .get('/get-skills?q=s')
+                        .expect(200)
+                        .expect(function(res)
+                        {
+                                var obj = JSON.parse(res.text);
+                                for(x in obj)
+                                        if(obj[x]["name"] == "Autism")
+                                                return;
+                                throw new Error("Missing skill 'Autism' for query 's'");
+                        })
+                .end(done)
+        });
+});
+
+describe('Carers can be retrieved', function()
+{
+        /*
+         * James Bond and Kiera Knightley possess skills 94, 98  
+         */
+        it('Returns the correct carers for a skillset', function(done)
+        {
+                request(app)
+                        .get('/search?skills=94,98')
+                        .expect(200)
+                        .expect(function(res)
+                        {
+                                var obj = JSON.parse(res.text);
+                                var flags = 0;
+                                for(x in obj)
+                                {
+                                        if(obj[x]["name"] == "James Bond")
+                                                flags++;
+                                        if(obj[x]["name"] == "Kiera Knightley")
+                                                flags++;
+                                }
+                                if(flags != 2)
+                                        throw new Error("Wrong carers returned for skillset");
+                        })
+                .end(done)
+        });
+
+        /*
+         * Postcode: SE13 5EB (random)
+         * Coords (lat/lon): 51.4612232818, -0.000499010074923
+         * Approx. distance from Bob Diamond with skill 81: 10km
+         * http://tinyurl.com/hkynm64
+         */
+        it('Distances are calculated correctly', function(done)
+        {
+                request(app)
+                        .get('/search?skills=81&target[]=-0.000499010074923&target[]=51.4612232818')
+                        .expect(200)
+                        .expect(function(res)
+                        {
+                                var obj = JSON.parse(res.text);
+                                for(x in obj)
+                                {
+                                        if(obj[x]["name"] == "Bob Diamond"
+                                                && obj[x]["distance"] < 12
+                                                && obj[x]["distance"] > 8)
+                                                break;
+                                        else throw new Error("Distance not quite right");
+                                }
+                        })
+                .end(done)
+        });
+});
